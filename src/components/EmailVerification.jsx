173e56ebd067c2b77ea7d3ecb6549e46f74223a7
Domguia-Simo <domguiasimoulrich@gmail.com>
2024-01-06 22:14:36 +0100

    arranging the votingForm, emailVerfication and more responsive on the landing page but without intergration

--- src/components/EmailVerification.jsx
+++ src/components/EmailVerification.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React,{useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import Header from './Header'
+
+// Styling
+import '../assets/styles/global.css'
 
 const EmailVerification=()=>{
+
+    const navigate = useNavigate()
+    const [loading ,setLoading] = useState(false)
+    const [code ,setCode] = useState(null)
+    const [error ,setError] = useState('')
+
+
+    async function sendConfirmation(){
+        console.log(code)
+        if(code == null){
+            return
+        }
+        setError('')
+        setLoading(true)
+        try{
+            fetch('http://lcalhost' ,{
+                method:'post',
+                headers:{
+                    'content-type':'application/json',
+                    'accept':'application/json',
+                    'access-control-origin':'*'
+                },
+                body:JSON.stringify({code:code})
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setLoading(false)
+                navigate('/')
+
+            })
+            .catch(err => {
+                console.log(err)
+                setError('Verify your internet connection')
+                setLoading(false)
+            })
+        }
+        catch(e){
+            console.log(e)
+            setError('Verify your internet connection')
+            setLoading(false)
+        }
+    }
+
     return(
         <React.Fragment>
-            email verification
+              <div  className='body'>
+                <Header/>
+                <form onSubmit={(e)=>e.preventDefault()}>
+
+                    <div
+                        style={{
+                            display:'flex',
+                            flexDirection:'column',
+                            alignItems:'center',
+                            rowGap:'5px',
+                            fontWeight:'bold',
+                            letterSpacing:1
+                        }}
+                    >
+                        <img src={require('../assets/images/logo.jpeg')} alt="logo" title='iai logo'
+                        className='iai-logo'
+                        />
+                        IAI COMEL
+                    </div>
+
+                    <div>
+                        {error != '' ? 
+                            <span style={{color:'darkred'}}>
+                                {error} &nbsp;
+                                <i className='fas fa-wifi' style={{textDecoration:'line-through' ,color:'darkred'}}></i>
+                            </span>
+                         :''}
+                    </div>
+
+                    <blockquote>
+                        - A code has been send to your email .<br/>-<b> Enter the code to register your vote </b>
+                    </blockquote>
+                    <input type="text" placeholder=' Verification code' value={code} name="code" onChange={e=>setCode(e.target.value)} required/>
+                    
+                    <button onClick={()=>sendConfirmation()} className='submit'>
+                        Confirm 
+                        {
+                            loading ? <img src={require('../assets/images/loader.gif')} width={'30px'} alt="loader"/>:''
+                        }
+                    </button>
+
+                </form>
+
+            </div>
         </React.Fragment>
     )
 }
--- src/components/VotingForm.jsx
+++ src/components/VotingForm.jsx
@@ -1,8 +1,26 @@
 import React ,{useState ,useEffect, useLayoutEffect} from 'react'
 import {useNavigate ,useLocation } from 'react-router-dom'
 
+import Header from './Header'
+
 // styling
 import '../assets/styles/global.css'
+
+const classes = [
+    {
+        level:"One",
+        list:["ba1a" ,"ba1b" ,"ba1c" ,"l1a" ,"l1b" ,'l1c','l1d','l1e','l1f','l1g','l1h','l1i','l1j','l1k',]
+    },
+    {
+        level:"Two",
+        list:[ "ba2a" ,"ba2b" ,'l2a','l2b','l2c','l2d','l2e','l2f','l2g','l2h']
+    },
+    {
+        level:"Three",
+        list:[ 'se3','sr3','gl1','gl2','gl3']
+    }
+]
+const levels = ["One" ,"Two" ,"Three"]
 
 const VotingForm=()=>{
 
@@ -11,27 +29,47 @@ const VotingForm=()=>{
         const [data ,setData] = useState({
             name:'',
             email:'',
-            class:'',
+            class:'ba1a',
             confirm:false
         })
         const [voted ,setVoted] = useState(useLocation().state)
         const [error ,setError] = useState('')
         const [loading ,setLoading] = useState(false)
+        const [level ,setLevel] = useState("One")
+        const [position ,setPosition] = useState({longitude:'' ,latitude:''})
 
         function handleChange(e){
-            console.log(e.target)
-            return
-            if(e.target.type === 'text' || e.target.type === 'email'){
+            // return
+            if(e.target.type == 'text' || e.target.type == 'email'){
                 setData({...data ,[e.target.name]:e.target.value})
             }else{
-                // setData({...data ,[e.target.name]:!e.target.value})
+                setData({...data ,[e.target.name]:!data.confirm})
             }
         }
 
+        async function findLocation(){
+            await window.navigator.geolocation.getCurrentPosition(point => {
+                // console.log(point.coords)
+                if(point.coords){
+                    setPosition({
+                        longitude:point.coords.longitude,
+                        latitude:point.coords.latitude
+                    })
+                }
+            })
+        }
+
         async function sendInfo(){
+            setError('')
+            let location
             if(data.name === '' || data.email === '' || data.class === '' || data.confirm === false){
-                setError("Please fill the entire form")
+                return
             }
+
+            location = await findLocation()
+            // comparing with the location of the school
+            navigate("/email-verification")
+return
             try{
                 setLoading(true)
                 let respond = await fetch('http://localhost:5000' ,{
@@ -44,46 +82,113 @@ const VotingForm=()=>{
                     body:JSON.stringify({
                         name:data.name,
                         email:data.email,
-                        class:data.class
+                        class:data.class,
+                        candidate:voted.name
                     })
                 })
-                let data = await respond.json()
-                console.log(data)
+                let res = await respond.json()
+                console.log(res)
+                navigate("email-verification")
                 setLoading(true)
 
             }
             catch(e){
                 console.log(e)
+                setError('Verify your internet connection')
                 setLoading(false)
             }
         }
 
+        let currentLevel
+        switch(level){
+            case "One":
+                currentLevel = 1
+            break;
+            case "Two":
+                currentLevel = 2
+            break;
+            case "Three":
+                currentLevel = 3
+            break;
+        }
+        let displayClasses = classes[currentLevel-1].list.map(classe => {
+            return(
+                <option key={classe}>{classe}</option>
+            )
+        })
+
+        let displayLevel = levels.map(level => {
+            return(
+                <option key={level}>{level}</option>
+            )
+        })
+
     return(
         <React.Fragment>
-            <div>
+            <div  className='body'>
+                <Header/>
                 <form onSubmit={(e)=>e.preventDefault()}>
 
                     <div
                         style={{
                             display:'flex',
+                            flexDirection:'column',
                             alignItems:'center',
-                            columnGap:'5px'
+                            rowGap:'5px',
+                            fontWeight:'bold',
+                            letterSpacing:1
                         }}
                     >
-                        <img src={require('../assets/images/logo.jpeg')} alt="logo" title='iai logo' width={'50px'} height={'50px'} style={{borderRadius:'10px'}}/>
+                        <img src={require('../assets/images/logo.jpeg')} alt="logo" title='iai logo'
+                        className='iai-logo'
+                        />
                         IAI COMEL
+                    </div>
+
+                    <div>
+                        {error ? 
+                            <span style={{color:'darkred'}}>
+                                {error} &nbsp;
+                                <i className='fas fa-wifi' style={{textDecoration:'line-through' ,color:'darkred'}}></i>
+                            </span>
+                         :''}
                     </div>
 
                     <input type="text" placeholder='Name' value={data.name} name="name" onChange={e=>handleChange(e)} required/>
                     <input type="email" placeholder='Email' value={data.email} name="email" onChange={e=>handleChange(e)} required/>
-                    <input type="text" placeholder='Class' value={data.class} name="class" onChange={e=>handleChange(e)} required/>
+                    
+                    <div style={{display:'flex' ,justifyContent:'space-between' ,alignItems:'center' ,columnGap:'20px'}}>
+                        <div>
+                            <select id="level" value={level} onChange={(e)=>setLevel(e.target.value)}>
+                                {displayLevel}
+                            </select>
+                            <label htmlFor='level'> level </label>
+                        </div>
+
+                        <div> 
+                            <select id="class" value={data.class} name="class" onChange={(e)=>setData({...data ,class:e.target.value})}> 
+                                {displayClasses}
+                            </select>
+                            <label htmlFor='class'> class </label>
+                        </div>
+                    </div>
+                    {/* <input type="text" placeholder='Class' value={data.class} name="class" onChange={e=>handleChange(e)} required/> */}
 
                     <div>
                         <input type="checkbox" value={data.confirm} id="confirm" name='confirm' onChange={e=>handleChange(e)} required/>
-                        <label htmlFor='confirm'>I Agree and confirm my vote</label>
+                        <label htmlFor='confirm' style={{textAlign:'center'}}>
+                            <span>I Agree and confirm my vote for</span> <br/> 
+                            <center><b> Mr. {voted.name} </b></center>
+                        </label>
                     </div>
 
-                <input type="submit" value="Vote" onClick={()=>sendInfo()}/>
+                    <button onClick={()=>sendInfo()} className='submit'>
+                        Vote 
+                        {
+                            loading ? <img src={require('../assets/images/loader.gif')} width={'30px'} alt="loader"/>:''
+                        }
+                    </button>
+
                 </form>
 
             </div>
